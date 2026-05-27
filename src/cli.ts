a95c1180ebde6c8f34f48919ab95d0d606d775c4
Your Name <you@example.com>
2026-05-27 16:50:34 -0300

    Atualização Código semi pronto

--- src/cli.ts
+++ src/cli.ts
@@ -1,22 +1,50 @@
 // CLI: Command Line Interface
+// import TodoListClass from "./core"
+// import { Item, TodoList } from "./core"
+// import { Item as Item1, TodoList as TodoList1 } from "./core"
 
 import TodoListClass, { Item } from "./core"
 
-const todoList = new TodoListClass("todoList.json")
+const todolist = new TodoListClass("todolist.json")
 const params = process.argv
 const command = params[2]
+
+// ------------------------------------------------------------------------------
+// --- Comando List
+// ------------------------------------------------------------------------------
+
+if (command === "list") {
+    const items = await todolist.getItems()
+    console.log("Lista de itens:")
+
+    // for (let index = 0; index < items.length; index++) {
+    //     const item = items[index] as Item
+    //     console.log(`${index}: ${item.title}`)
+    // }
+
+    items.forEach((item, index) => {
+        console.log(`${index}: ${item.title}`)
+    })
+
+    process.exit(0)
+}
+
+// ------------------------------------------------------------------------------
+// --- Comando Add
+// ------------------------------------------------------------------------------
 
 if (command === "add") {
     const value = params[3]
 
     if (!value) {
-        console.log("aqui vai a lógica de adicionar: " + value)
+        console.error("Valor do item é obrigatório")
         process.exit(1)
     }
+
     try {
-        await todoList.addItem(new Item(value))
+        await todolist.addItem(new Item(value))
     } catch (error) {
-        console.error("Erro ao adicionar item", error)
+        console.error("Erro ao adicionar item:", error)
         process.exit(1)
     }
 
@@ -24,12 +52,35 @@ if (command === "add") {
     process.exit(0)
 }
 
+// ------------------------------------------------------------------------------
+// --- Comando Remove
+// ------------------------------------------------------------------------------
+
+if (command === "remove") {
+    const indexStr = params[3]
+    if (!indexStr) {
+        console.error("Índice do item é obrigatório")
+        process.exit(1)
+    }
+    const index = parseInt(indexStr)
+    if (isNaN(index)) {
+        console.error("Índice inválido, precisa ser um número inteiro")
+        process.exit(1)
+    }
+    await todolist.removeItem(index)
+    console.log(`Item no índice ${index} removido com sucesso!`)
+    process.exit(0)
+}
+
+// ------------------------------------------------------------------------------
+// --- Fallback para comandos não reconhecidos
+// ------------------------------------------------------------------------------
+
 if (command)
-    console.log(`Comando não reconhecido: ${command}`);
-    
+    console.log(`Comando não reconhecido: ${command}`)
 
 console.log(`Comandos disponíveis:
-    - add <item>: Adiciona um item a lista
-    - remove <index>: remove um item da lista por indice
-    - list: Lista os itens atuais
-`);
+- add <item>: Adiciona um item à lista
+- remove <index>: Remove um item da lista por indice
+- list: Lista os itens atuais
+`)
--- src/core.ts
+++ src/core.ts
@@ -12,55 +12,63 @@
 // }
 
 class Item {
-    constructor(public title: string) { }
+  constructor(public title: string) { }
 }
 
 class TodoList {
-    private items: Item[] = []
-    private filePath: string
+  private items: Promise<Item[]>
+  private filePath: string
 
-    constructor(filePath: string) {
-        this.filePath = filePath
-        this.loadListFromDisk()
-    }
+  constructor(filePath: string) {
+    this.filePath = filePath
+    this.items = this.loadListFromDisk()
+  }
 
-    private async saveListToDisk() {
-        const file = Bun.file(this.filePath)
-        const data = JSON.stringify(this.items)
-        await file.write(data)
-    }
+  private async saveListToDisk() {
+    const file = Bun.file(this.filePath)
+    const data = JSON.stringify(await this.items)
+    await file.write(data)
+  }
 
-    private async loadListFromDisk() {
-        const file = Bun.file(this.filePath)
-        // const text = await file.text()
-        // const data = JSON.parse(text)
-        const data = await file.json();
-        this.items = data.map((v: any) => new Item(v.title))
-    }
+  private async loadListFromDisk() {
+    const file = Bun.file(this.filePath)
+    // const text = await file.text()
+    // const data = JSON.parse(text)
+    const data = await file.json() as Item[]
+    const items = data.map((v: any) => new Item(v.title))
+    return items
+  }
 
-    /**
-     * Função que adiciona um novo item a lista
-     */
-    async addItem(item: Item) {
-        this.items.push(item)
-        await this.saveListToDisk()
-    }
+  /**
+   * Função que adiciona um novo item a lista
+   */
+  async addItem(item: Item) {
+    const items = await this.items
+    if (!item) 
+      throw "Item inválido"
+    if (!item.title.trim())
+      throw "Item deve conter um título"
+    items.push(item)
+    await this.saveListToDisk()
+  }
 
-    /**
-     * Remove item da lista por um indice
-     */
-    async removeItem(index: number) {
-        this.items.splice(index, 1)
-        await this.saveListToDisk()
-    }
+  /**
+   * Remove item da lista por um indice
+   */
+  async removeItem(index: number) {
+    const items = await this.items
+    items.splice(index, 1)
+    await this.saveListToDisk()
+  }
 
-    /**
-     * Retorna a cópia da lista de itens
-     */
-    getItems() {
-        return Array.from(this.items)
-    }
+  /**
+   * Retorna a cópia da lista de itens
+   */
+  async getItems() {
+    const items = await this.items
+    return Array.from(items)
+  }
 }
 
 export default TodoList
-export { Item, TodoList }
+export { TodoList, Item }
